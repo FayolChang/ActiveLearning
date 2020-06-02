92d3ed5d18935723adffb1b5ac1c92fddf5cfd76
waka <liangnida@163.com>
2020-06-02 15:34:06 +0800

    change sampling method

--- active_learning/main.py
+++ active_learning/main.py
@@ -1,9 +1,9 @@
 from active_learning import train, infer, prepare_data
-from active_learning.train import train_main
 
-for p in [20, 40, 60, 80]:
+for p in [20, 40, 60, 80, 100]:
     train.train_main(p)
     infer.infer_main(100-p)
+    if p == 100: continue
     prepare_data.select_samples(p, 100-p, p+20, 80-p)
 
-train_main('train_data.json')
+# train_main('train_data.json')
--- active_learning/prepare_data.py
+++ active_learning/prepare_data.py
@@ -96,15 +96,21 @@ def select_samples(labeled_file, unlabeled_file, o_labeled_file, o_unlabeled_fil
     o_unlabeled_file = f'unlabeled_{o_unlabeled_file}.json'
 
     # low_cnt, random_cnt = 5400, 340
-    low_cnt, random_cnt = 4000, 1740
+    # low_cnt, random_cnt = 4000, 1740
 
     d_unlabeled = json.load((Path(data_dir) / unlabeled_file).open())
     d_labeled = json.load((Path(data_dir) / labeled_file).open())
 
+    mid = len(d_unlabeled) // 2
+    span = (len(d_labeled) + len(d_unlabeled)) // 10
+    start = max(0, mid - span)
+    end = mid + span
+
     d_unlabeled = sorted(d_unlabeled, key=lambda x: float(x['score']))
 
-    low_samples, d_unlabeled = d_unlabeled[:low_cnt], d_unlabeled[low_cnt:]
-    random_samples = random.sample(d_unlabeled, random_cnt)
+    low_samples, d_unlabeled = d_unlabeled[start: end], d_unlabeled[: start] + d_unlabeled[end:]
+    # random_samples = random.sample(d_unlabeled, random_cnt)
+    random_samples = []
     for _ in random_samples:
         d_unlabeled.remove(_)
 
@@ -141,7 +147,7 @@ def select_samples(labeled_file, unlabeled_file, o_labeled_file, o_unlabeled_fil
     # d_to_add + d_labeled size: 22938
 
 
-
+select_samples(20, 80, 40, 60)
 
 
 
