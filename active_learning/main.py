b8a1fea9b88749dc3521429e911111a33e8c6e62
waka <liangnida@163.com>
2020-06-02 11:37:33 +0800

    main

--- active_learning/main.py
+++ active_learning/main.py
@@ -2,8 +2,8 @@ from active_learning.infer import infer_main
 from active_learning.prepare_data import select_samples
 from active_learning.train import train_main
 
-# for p in [20, 40, 60, 80]:
-for p in [20]:
+for p in [20, 40, 60, 80]:
+# for p in [20]:
     train_main(p)
     infer_main(100-p)
     select_samples(p, 100-p, p+20, 80-p)
