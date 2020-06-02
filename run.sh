50155e174e6e7a977fab0c051cb393eb44d4883b
waka <liangnida@163.com>
2020-06-02 10:50:27 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 export PYTHONPATH="."
 
-/home/lwty/anaconda3/envs/py36/bin/python active_learning/train.py  --per_gpu_batch_size 8
-/home/lwty/anaconda3/envs/py36/bin/python active_learning/infer.py  --per_gpu_batch_size 8
+/home/lwty/anaconda3/envs/py36/bin/python active_learning/main.py  --per_gpu_batch_size 8
+#/home/lwty/anaconda3/envs/py36/bin/python active_learning/infer.py  --per_gpu_batch_size 8
 
