f317644b8d2f5e1da21a2dca630c43b47c83acb0
waka <liangnida@163.com>
2020-06-02 22:25:35 +0800

    main

--- active_learning/main.py
+++ active_learning/main.py
@@ -4,6 +4,6 @@ for p in [20, 40, 60, 80, 100]:
     train.train_main(p)
     infer.infer_main(100-p)
     if p == 100: continue
-    prepare_data.select_samples(p, 100-p, p+20, 80-p)
+    prepare_data.select_samples(p, 100-p, p+20, 80-p, query_strategy='low')
 
 # train_main('train_data.json')
